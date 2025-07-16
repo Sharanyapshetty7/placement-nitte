61b4821e3be2a3ea29ec8264ac6a5ebdedbbd321
Sharanya  p shetty <159996195+Sharanyapshetty7@users.noreply.github.com>
2025-07-16 06:22:51 +0530

    Create starpattern.py

---
+++ starpattern.py
@@ -0,0 +1,13 @@
+# Top Line
+for i in range(10):
+    print("*", end=" ")
+print()
+
+# Middle Rectangle
+for i in range(10):
+    print("*                 *")
+
+# Bottom Line
+for i in range(10):
+    print("*", end=" ")
+print()
